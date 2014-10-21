7e410dc7a571862880365ca10e3ec249bc61a0b5
jaredscheib <jared.scheib@gmail.com>
2014-10-20 22:26:50 -0700

    (fix) Retrieve current URL from chrome tab

---
+++ background.js
@@ -0,0 +1,35 @@
+// Letsfix Chrome Extension
+// 
+// Share URL with crowd.
+// Connect with other who are feeling what you're feeling at that exact moment.
+
+
+var post = function(url, title){
+  // TODO: Gmail OAuth connection
+  var message = {to: 'problems@letsfix.net', subject: title, message: url};
+  // gmailSend(message); //not finished
+  console.log(message);
+};
+
+var gmailSend = function(content){
+
+
+  // doesn't work
+  //  return GmailIMAPlibrary;
+};
+
+var lfxURL = function(){
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
+    var url = tabs[0].url;
+    var title = tabs[0].title;
+    console.log('TABS', tabs);
+    post(url, title);
+  });
+};
+
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var action_url = lfxURL();
+  chrome.tabs.update(tab.id, {url: action_url});
+});
